e63b481659bbb0b5e16c4896328a5781aff132f0
IanPC <iancaminhas25@hotmail.com>
2023-06-21 09:51:52 -0300

    Usando fontes personalizadas - Google fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { Home } from './src/pages/Home';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 const App: React.FunctionComponent = () => {
+  //Aqui já tenho as duas fontes disponíveis para trabalhar no projeto
+  //fontsLoaded é um array que carrega o momento em que a fonte foi carregada no projeto
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+  //A tela de splash screen vai ficar mantida enquanto as fontes não forem carregadas. Retorna esse componente até as fontes serem carregadas
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  //antes de retornar toda a aplicação, preciso carregar as fontes referentes às instruções acima
   return (
     <ThemeProvider theme={theme}>
       <Home />
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,19 +3,22 @@ Se fosse styled-components web seria 'styled-components
 */
 import styled from 'styled-components/native';
 
-//estou criando um componente dizendo que ele é igual a uma View. Mas essa View é estilizada pelo styled-components
-//No final das contas, um componente vai ser criado
-export const Container = styled.View`
+/*estou criando um componente dizendo que ele é igual a uma View. Mas essa View é estilizada pelo styled-components
+No final das contas, um componente vai ser criado
   flex: 1; //quero que ocupe 100% da tela
   justify-content: center; //conteudo centralizado
   align-items: center; //centralizado tanto verticalmente e horizontalmente
+*/
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
   background-color: ${({ theme }) => theme.colors.dark};
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: ${({ theme }) =>
-    theme.colors
-      .light}; //{props => props.theme.colors.light} só usei a desestruturação
+  color: ${({ theme }) => theme.colors.light};
 `;
