165569944bd12a3e67498627511b8f50d4d09524
IanPC <iancaminhas25@hotmail.com>
2023-06-21 11:55:54 -0300

    Criando cabecalho da pagina Home - parte 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Container, Header } from './styles';
 
+//se o usuario não tiver a imagem no back-end, vou usar o avatar default
+import avatarDefault from '../../assets/avatar02.png';
+
 export const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Header />
+      <Header>
+        <Userwrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={}>
+                <userAvatar source={avatarDefault} />
+            </ UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Ian</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+        </Userwrapper>
+      </Header>
     </Container>
   );
 };
