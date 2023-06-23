7fd2eb98be9586774258803cb78d8961138b196a
IanPC <iancaminhas25@hotmail.com>
2023-06-23 14:27:17 -0300

    Criando o componente Button - parte 1

---
+++ src/components/Form/Button/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Title } from './styles';
+import { TouchableOpacityProps } from 'react-native';
+
+//Alem de extender as propriedades do TouchableOpacityProps, vou acrescentar a propriedade title
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+/*
+  declaro um tipo genérico <ButtonProps>... Esse genérico é do próprio react-native
+  A partir de agora vou ter todas as propriedades de um ButtonProps para serem usadas...
+  Esse otherProps são as propriedades passadas por outros componentes. Essas propriedades podem variar
+  Input agora é reutilizável
+  além do title do botão, pego todas as outras propriedades com ..otherProps
+*/
+export const Button: React.FunctionComponent<ButtonProps> = ({
+  title,
+  ...otherProps
+}) => {
+  return (
+    <Container {...otherProps}>
+      <Title>{Title}</Title>
+    </Container>
+  );
+};
