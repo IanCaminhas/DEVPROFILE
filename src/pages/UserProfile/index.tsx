fdb7a4949df18d19c19b09e6bce7cedd6ae54fcc
IanPC <iancaminhas25@hotmail.com>
2023-07-29 09:07:50 -0300

    Criando a página de perfil do usuário - parte 1

---
+++ src/pages/UserProfile/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Content,
+  EmailData,
+  EmailTitle,
+  GoBackButton,
+  Header,
+  HeaderTitle,
+  Icon,
+  NameData,
+  NameTitle,
+  PhotoButton,
+  PhotoContainer,
+  UserAvatar,
+  UserEmailDetail,
+  UserNameDetail,
+} from './styles';
+import avatarDefault from '../../assets/avatar02.png';
+import { useAuth } from '../context/AuthContext';
+
+interface ScreenNavigationProp {
+  //volta para a tela anterior
+  goBack: () => void;
+}
+
+export const UserProfile: React.FunctionComponent = () => {
+  const { user } = useAuth();
+  const { goBack } = useNavigation<ScreenNavigationProp>();
+
+  return (
+    <Container>
+      <Header>
+        <GoBackButton onPress={goBack}>
+          <Icon name="chevron-left" />
+        </GoBackButton>
+        <HeaderTitle>Seu perfil</HeaderTitle>
+      </Header>
+      <PhotoContainer>
+        <UserAvatar
+          source={user.avatar_url ? { uri: user.avatar_url } : avatarDefault}
+        />
+        <PhotoButton>
+          <Icon name="camera" />
+        </PhotoButton>
+      </PhotoContainer>
+      <Content>
+        <UserNameDetail>
+          <NameTitle>NAME</NameTitle>
+          <NameData>{user.name}</NameData>
+        </UserNameDetail>
+        <UserEmailDetail>
+          <EmailTitle>EMAIL</EmailTitle>
+          <EmailData>{user.email}</EmailData>
+        </UserEmailDetail>
+      </Content>
+    </Container>
+  );
+};
